e779b1365c4eb7064670eb4754493283ee2ebe3e
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-12-30 21:58:30 -0300

    little adjust

--- src/data.js
+++ src/data.js
@@ -4,18 +4,18 @@ export const data = [
         id: 1,
         name: "Karla Newman",
         age: 35,
-        image: "../imgs/friend1.jpg"
+        image: "../public/friend1.jpg"
     },
     {
         id: 2,
         name: "Amanda Miller",
         age: 23,
-        image: "../imgs/friend2.jpg"
+        image: "../public/friend2.jpg"
     },
     {
         id: 3,
         name: "Zoe Reeves",
         age: 18,
-        image: "../imgs/friend3.jpg"
+        image: "../public/friend3.jpg"
     }
 ]
