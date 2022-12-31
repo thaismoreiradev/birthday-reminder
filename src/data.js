3e8f047e59b89597949e5702136820b9ade73aad
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-12-30 22:32:32 -0300

    uploading photos in aws

--- src/data.js
+++ src/data.js
@@ -4,18 +4,18 @@ export const data = [
         id: 1,
         name: "Karla Newman",
         age: 35,
-        image: "../public/friend1.jpg"
+        image: "https://profile-images-options.s3.sa-east-1.amazonaws.com/friend1.jpg"
     },
     {
         id: 2,
         name: "Amanda Miller",
         age: 23,
-        image: "../public/friend2.jpg"
+        image: "https://profile-images-options.s3.sa-east-1.amazonaws.com/friend2.jpg"
     },
     {
         id: 3,
         name: "Zoe Reeves",
         age: 18,
-        image: "../public/friend3.jpg"
+        image: "https://profile-images-options.s3.sa-east-1.amazonaws.com/friend3.jpg"
     }
 ]
